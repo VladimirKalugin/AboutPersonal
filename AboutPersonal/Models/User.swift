3166e847a0458d39d261b66708928ab47e7d1e82
Vladimir Kalugin <kaluginbc@gmail.com>
2020-08-30 13:09:56 +0700

    create the model file

---
+++ AboutPersonal/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  AboutPersonal
+//
+//  Created by Fuhrer_SS on 30.08.2020.
+//  Copyright © 2020 myself. All rights reserved.
+//
+
+struct User {
+    let userName: String
+    let userPassword: String
+}
+
+let newUser = User(userName: "User", userPassword: "Password")
+
+
