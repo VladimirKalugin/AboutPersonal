c301016bf618c703d52a7b6770056b5044219757
Vladimir Kalugin <kaluginbc@gmail.com>
2020-08-30 12:33:26 +0700

    Initial Commit

---
+++ AboutPersonal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutPersonal
+//
+//  Created by Fuhrer_SS on 30.08.2020.
+//  Copyright © 2020 myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
